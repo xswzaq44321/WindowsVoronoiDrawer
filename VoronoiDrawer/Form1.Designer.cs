f68de163bcd706f5d1f3da45e8d2be0a52e5681f
Weber <xswzaq44321@gmail.com>
2018-11-15 21:52:44 +0800

    first commit

--- VoronoiDrawer/Form1.Designer.cs
+++ VoronoiDrawer/Form1.Designer.cs
@@ -28,12 +28,109 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.components = new System.ComponentModel.Container();
+			this.pictureBox = new System.Windows.Forms.PictureBox();
+			this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+			this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.clearMapToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+			this.outputImageToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+			this.menuStrip1.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// pictureBox
+			// 
+			this.pictureBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+			this.pictureBox.Location = new System.Drawing.Point(12, 34);
+			this.pictureBox.Name = "pictureBox";
+			this.pictureBox.Size = new System.Drawing.Size(600, 600);
+			this.pictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+			this.pictureBox.TabIndex = 0;
+			this.pictureBox.TabStop = false;
+			this.pictureBox.DragDrop += new System.Windows.Forms.DragEventHandler(this.pictureBox_DragDrop);
+			this.pictureBox.DragEnter += new System.Windows.Forms.DragEventHandler(this.pictureBox_DragEnter);
+			// 
+			// menuStrip1
+			// 
+			this.menuStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+			this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+			this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+			this.menuStrip1.Name = "menuStrip1";
+			this.menuStrip1.Size = new System.Drawing.Size(635, 32);
+			this.menuStrip1.TabIndex = 1;
+			this.menuStrip1.Text = "menuStrip1";
+			// 
+			// fileToolStripMenuItem
+			// 
+			this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem,
+            this.clearMapToolStripMenuItem,
+            this.outputImageToolStripMenuItem});
+			this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+			this.fileToolStripMenuItem.Size = new System.Drawing.Size(86, 28);
+			this.fileToolStripMenuItem.Text = "Control";
+			// 
+			// openToolStripMenuItem
+			// 
+			this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+			this.openToolStripMenuItem.Size = new System.Drawing.Size(222, 30);
+			this.openToolStripMenuItem.Text = "Open new map";
+			this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
+			// 
+			// clearMapToolStripMenuItem
+			// 
+			this.clearMapToolStripMenuItem.Name = "clearMapToolStripMenuItem";
+			this.clearMapToolStripMenuItem.Size = new System.Drawing.Size(222, 30);
+			this.clearMapToolStripMenuItem.Text = "Clear map";
+			this.clearMapToolStripMenuItem.Click += new System.EventHandler(this.clearMapToolStripMenuItem_Click);
+			// 
+			// openFileDialog1
+			// 
+			this.openFileDialog1.FileName = "openFileDialog1";
+			this.openFileDialog1.Filter = "Vornoi Map (*.json) | *.json";
+			// 
+			// outputImageToolStripMenuItem
+			// 
+			this.outputImageToolStripMenuItem.Name = "outputImageToolStripMenuItem";
+			this.outputImageToolStripMenuItem.Size = new System.Drawing.Size(222, 30);
+			this.outputImageToolStripMenuItem.Text = "Save Image";
+			this.outputImageToolStripMenuItem.Click += new System.EventHandler(this.outputImageToolStripMenuItem_Click);
+			// 
+			// saveFileDialog1
+			// 
+			this.saveFileDialog1.Filter = "JPeg Image|*.jpg|Bitmap Image|*.bmp|Gif Image|*.gif|Png Image|*.png";
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(635, 656);
+			this.Controls.Add(this.pictureBox);
+			this.Controls.Add(this.menuStrip1);
+			this.MainMenuStrip = this.menuStrip1;
+			this.Name = "Form1";
 			this.Text = "Form1";
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+			this.menuStrip1.ResumeLayout(false);
+			this.menuStrip1.PerformLayout();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
 		}
 
 		#endregion
+
+		private System.Windows.Forms.PictureBox pictureBox;
+		private System.Windows.Forms.MenuStrip menuStrip1;
+		private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem clearMapToolStripMenuItem;
+		private System.Windows.Forms.OpenFileDialog openFileDialog1;
+		private System.Windows.Forms.ToolStripMenuItem outputImageToolStripMenuItem;
+		private System.Windows.Forms.SaveFileDialog saveFileDialog1;
 	}
 }
 
--- VoronoiDrawer/Form1.cs
+++ VoronoiDrawer/Form1.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Newtonsoft.Json;
+using System.IO;
 
 namespace VoronoiDrawer
 {
@@ -15,6 +17,123 @@ namespace VoronoiDrawer
 		public Form1()
 		{
 			InitializeComponent();
+			blueBrush = new SolidBrush(Color.Blue);
+			BlackPen = new Pen(Color.Black, 3);
+			circleSize = new Size(7, 7);
+			pictureBox.AllowDrop = true;
+
+			readMap("test.json", out vmap);
+			drawVoronoi(vmap);
+		}
+
+		Bitmap bmp;
+		Graphics g;
+		Pen BlackPen;
+		Brush blueBrush;
+		Size circleSize;
+		VoronoiStruct.Voronoi vmap;
+
+		void readMap(string filePath, out VoronoiStruct.Voronoi map)
+		{
+			StreamReader sr = new StreamReader(filePath);
+			string json = sr.ReadToEnd();
+			map = JsonConvert.DeserializeObject<VoronoiStruct.Voronoi>(json);
+		}
+
+		void drawPoint(VoronoiStruct.Point pos)
+		{
+			// this func is just a short hand for writting FillEllipse
+			Point pos1 = new Point(pos.x, pos.y);
+			g.FillEllipse(blueBrush, new RectangleF(pos1, circleSize));
+		}
+		void drawPoint(Point pos)
+		{
+			g.FillEllipse(blueBrush, new RectangleF(pos, circleSize));
+		}
+
+		void drawLine(VoronoiStruct.Point pos1, VoronoiStruct.Point pos2)
+		{
+			g.DrawLine(BlackPen, pos1.x, pos1.y, pos2.x, pos2.y);
+		}
+		void drawLine(Point pos1, Point pos2)
+		{
+			g.DrawLine(BlackPen, pos1, pos2);
+		}
+
+		void drawVoronoi(VoronoiStruct.Voronoi map)
+		{
+			bmp = new Bitmap(map.width, map.height);
+			g = Graphics.FromImage(bmp);
+			g.Clear(Color.White);
+			pictureBox.Image = bmp;
+			foreach (var item in map.points)
+			{
+				drawPoint(item);
+			}
+			foreach (var item in map.lines)
+			{
+				drawLine(item.a, item.b); ;
+			}
+		}
+
+		private void openToolStripMenuItem_Click(object sender, EventArgs e)
+		{
+			if (openFileDialog1.ShowDialog() == DialogResult.OK)
+			{
+				readMap(openFileDialog1.FileName, out vmap);
+				drawVoronoi(vmap);
+			}
+		}
+
+		private void clearMapToolStripMenuItem_Click(object sender, EventArgs e)
+		{
+			g.Clear(Color.White);
+			pictureBox.Invalidate();
+		}
+
+		private void pictureBox_DragEnter(object sender, DragEventArgs e)
+		{
+			if (e.Data.GetDataPresent(DataFormats.FileDrop))
+			{
+				e.Effect = DragDropEffects.Copy;
+			}
+		}
+
+		private void pictureBox_DragDrop(object sender, DragEventArgs e)
+		{
+			string[] path = (string[])e.Data.GetData(DataFormats.FileDrop, false);
+			if (Path.GetExtension(path[0]) == ".json")
+			{
+				readMap(path[0], out vmap);
+				drawVoronoi(vmap);
+			}
+		}
+
+		private void outputImageToolStripMenuItem_Click(object sender, EventArgs e)
+		{
+			if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+			{
+				FileStream fs = (FileStream)saveFileDialog1.OpenFile();
+				switch (saveFileDialog1.FilterIndex)
+				{
+					case 1:
+						pictureBox.Image.Save(fs,
+							System.Drawing.Imaging.ImageFormat.Jpeg);
+						break;
+					case 2:
+						pictureBox.Image.Save(fs,
+							System.Drawing.Imaging.ImageFormat.Bmp);
+						break;
+					case 3:
+						pictureBox.Image.Save(fs,
+							System.Drawing.Imaging.ImageFormat.Gif);
+						break;
+					case 4:
+						pictureBox.Image.Save(fs,
+							System.Drawing.Imaging.ImageFormat.Png);
+						break;
+				}
+			}
 		}
 	}
 }
