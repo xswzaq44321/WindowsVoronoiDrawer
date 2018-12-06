d684f896894f58aa870eb8d4f324dbc8119d15d5
Weber <xswzaq44321@gmail.com>
2018-12-06 18:04:38 +0800

    Add Arc class & cross product calculation

--- VoronoiDrawer/Fortune.cs
+++ VoronoiDrawer/Fortune.cs
@@ -522,6 +522,13 @@ namespace VoronoiDrawer
 			this.X = center.X + r;
 			isCircle = true;
 		}
+		public Event(Event old)
+		{
+			this.relevant = new List<Polygon>(old.relevant);
+			this.X = old.X;
+			this.center = old.center;
+			this.isCircle = old.isCircle;
+		}
 
 		public List<Polygon> relevant = new List<Polygon>();
 		public double X;
@@ -536,6 +543,68 @@ namespace VoronoiDrawer
 				return 0;
 			else
 				return 1;
+		}
+	}
+
+	class Arc
+	{
+		public Arc(Polygon parent)
+		{
+			this.focus = parent.focus;
+			this.parent = parent;
+		}
+		public Arc(Arc old, bool withEvent = true)
+		{
+			this.focus = old.focus;
+			this.parent = old.parent;
+			if (withEvent)
+			{
+				this.e = new Event(old.e);
+			}
+		}
+		public VoronoiStruct.Point focus;
+		public Event e;
+		public Polygon parent;
+		public PointF getIntersect(Arc obj, double L)
+		{
+			double ka = this.focus.y;
+			double ha = (L + this.focus.x) / 2.0;
+			double ca = -(L - this.focus.x) / 2.0;
+			double kb = obj.focus.y;
+			double hb = (L + obj.focus.x) / 2.0;
+			double cb = -(L - obj.focus.x) / 2.0;
+			double a = cb - ca;
+			double b = -2 * (cb * ka - ca * kb);
+			double c = -(4 * ca * cb * (hb - ha) - cb * ka * ka + ca * kb * kb);
+			double distance = b * b - 4 * a * c;
+			if (a == 0)
+			{
+				if (b == 0)
+				{
+					return new PointF();
+				}
+				double y = -c / b;
+				double x = Math.Pow(y - ka, 2) / (4 * ca) + ha;
+				return new PointF((float)x, (float)y);
+			}
+			else
+			{
+				double[] root = new double[2];
+				root[0] = (-b + Math.Sqrt(distance)) / (2 * a);
+				root[1] = (-b - Math.Sqrt(distance)) / (2 * a);
+				Array.Sort(root);
+				double y;
+				if (this.focus.x < obj.focus.x)
+				{
+					y = root[0];
+				}
+				else
+				{
+					y = root[1];
+				}
+				double x = Math.Pow(y - ka, 2) / (4 * ca) + ha;
+				return new PointF((float)x, (float)y);
+			}
 		}
 	}
 
@@ -555,6 +624,47 @@ namespace VoronoiDrawer
 		public static double distance(PointF a, PointF b)
 		{
 			return Math.Sqrt(Math.Pow(a.X - b.X, 2) + Math.Pow(a.Y - b.Y, 2));
+		}
+		// 向量oa與向量ob進行叉積，判斷oa到ob的旋轉方向。
+		public static double cross(VoronoiStruct.Point o, VoronoiStruct.Point a, VoronoiStruct.Point b)
+		{
+			return (double)(a.x - o.x) * (b.y - o.y) - (double)(a.y - o.y) * (b.x - o.x);
+		}
+		// 點與線段已確定共線，判斷相交。
+		public static bool intersect(VoronoiStruct.Point p1, VoronoiStruct.Point p2, VoronoiStruct.Point p)
+		{
+			return p.x >= Math.Min(p1.x, p2.x)
+				&& p.x <= Math.Max(p1.x, p2.x)
+				&& p.y >= Math.Min(p1.y, p2.y)
+				&& p.y <= Math.Max(p1.y, p2.y);
+		}
+		public static bool intersect(VoronoiStruct.Point a1, VoronoiStruct.Point a2, VoronoiStruct.Point b1, VoronoiStruct.Point b2)
+		{
+			double c1 = cross(a1, a2, b1);
+			double c2 = cross(a1, a2, b2);
+			double c3 = cross(b1, b2, a1);
+			double c4 = cross(b1, b2, a2);
+			// 端點不共線
+			if (c1 * c2 < 0 && c3 * c4 < 0) return true;
+			// 端點共線
+			if (c1 == 0 && intersect(a1, a2, b1)) return true;
+			if (c2 == 0 && intersect(a1, a2, b2)) return true;
+			if (c3 == 0 && intersect(b1, b2, a1)) return true;
+			if (c4 == 0 && intersect(b1, b2, a2)) return true;
+			return false;
+		}
+		public static bool intersect(Line a, Line b)
+		{
+			return intersect(a.a, a.b, b.a, b.b);
+		}
+		public static bool intersect(Rectangle regin, Line line)
+		{
+			Line top = new Line(regin.Left, regin.Top, regin.Right, regin.Top);
+			Line bottom = new Line(regin.Left, regin.Bottom, regin.Right, regin.Bottom);
+			Line left = new Line(regin.Left, regin.Top, regin.Left, regin.Bottom);
+			Line right = new Line(regin.Right, regin.Top, regin.Right, regin.Bottom);
+			return intersect(top, line) || intersect(bottom, line)
+				|| intersect(left, line) || intersect(right, line);
 		}
 	}
 }
